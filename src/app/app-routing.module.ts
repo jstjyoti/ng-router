00a836fb563a34008e114ae70103696514ae8027
Jyoti Mishra <jyoti@flytbase.com>
2020-06-01 16:41:14 +0530

    parsing server side links for router without htmlHistory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,7 @@ const appRoutes: Routes = [
 
   @NgModule ({
     imports: [
-        RouterModule.forRoot(appRoutes)
+        RouterModule.forRoot(appRoutes, {useHash: true})
     ],
     exports: [RouterModule]
   }) export class AppRouterModule { }
