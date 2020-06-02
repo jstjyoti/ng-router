4fd13725cfaf853799fa804d740f8173946e82dc
Jyoti Mishra <jyoti@flytbase.com>
2020-06-02 22:20:11 +0530

    service modified

--- src/app/can-deactivate-guard.service.ts
+++ src/app/can-deactivate-guard.service.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs';
 import {ActivatedRouteSnapshot, RouterStateSnapshot, CanDeactivate } from '@angular/router';
 
 export interface CanComponentDeactivate {
-    canDeactivate(): () => Observable<boolean> | Promise<boolean> | boolean;
+    canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
 }
 
 export class CanDeactivateGuard implements CanDeactivate<CanComponentDeactivate> {
@@ -11,6 +11,6 @@ export class CanDeactivateGuard implements CanDeactivate<CanComponentDeactivate>
         currentState: RouterStateSnapshot,
         nextSate?: RouterStateSnapshot
         ): Observable<boolean> | Promise<boolean> | boolean {
-        return component.canDeactivate()();
+        return component.canDeactivate();
     }
 }
